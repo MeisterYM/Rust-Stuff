66e191ae1e28703b3e91d34614174f61e3c6cf86
MeisterYM <yannick_mart@yahoo.com>
2024-05-13 16:47:34 -0500

    First commit

---
+++ Math.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    //let something = "What?";
+
+    //WHY IS THIS THE FORMAT FOR STRINGS?
+    //println!("{}", something);
+
+    let number = 2;
+
+    if number == 2 {
+        println!("TWO!?");
+    } else {
+        println!("NOT TWO!?");
+    }
+}
